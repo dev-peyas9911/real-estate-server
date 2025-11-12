4589279f9b77c73e64f14288af42179c7623c405
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2025-11-12 17:31:57 +0600

    set up all file

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+// username: mdoel-db
+// password: ONX37JFwQpMgjAC6
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://model-db:ONX37JFwQpMgjAC6@cluster0.u7wqc8p.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`This server is running on port ${port}`);
+});
+
